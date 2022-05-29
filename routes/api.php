d376457eb49a10b6409708a049794219a5621c59
Felix Melendez <ivanwolf007@gmail.com>
2022-05-28 18:13:15 -0600

    Agregando comentarios a endpoints para tener una mejor guia de uso

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 /*Rutas para acceder a informacion de peliculas */
 Route::get('/peliculas', 'App\Http\Controllers\PeliculaController@index'); //Mostrar todos los registros de peliculas
+/*
+Ejemplo de endpoint: http://127.0.0.1:8000/api/peliculas       
+*/
 Route::get('/peliculas/{idPeliculas}', 'App\Http\Controllers\PeliculaController@show'); //Mostrar registro de busqueda de pelicula con id
+/*
+Ejemplo de endpoint: http://127.0.0.1:8000/api/peliculas/2       
+*/
 Route::get('/peliculas/search/{nombre}', 'App\Http\Controllers\PeliculaController@searchMovie'); //Mostrar registro de busqueda de pelicula con nombre
+/*
+Ejemplo de endpoint: http://127.0.0.1:8000/api/peliculas/spider       
+*/
 
 /*Rutas para acceder a informacion de usuarios */
 Route::get('/usuarios', 'App\Http\Controllers\UsuariosController@index'); //Mostrar todos los registros de usuarios
-Route::post('/usuarios', 'App\Http\Controllers\UsuariosController@store'); //Mostrar todos los registros de usuarios
+/*
+Ejemplo de endpoint: http://127.0.0.1:8000/api/usuarios       
+*/
+Route::post('/usuarios', 'App\Http\Controllers\UsuariosController@store'); //Registrar usuario por metodo post
+/*
+Ejemplo de endpoint: http://127.0.0.1:8000/api/usuarios       
+*/
 
 /*Rutas para acceder a informacion de login */
 Route::get('/login/{user}/{pass}', 'App\Http\Controllers\UsuariosController@login'); //Login
+/*
+Ejemplo de endpoint: http://127.0.0.1:8000/api/login/felix/1234f0       
+*/
